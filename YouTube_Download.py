1f779f6daa7b421138357add1fffbc50c269bc8a
Sahil <sahilsinghai5672@gmail.com>
2022-10-08 15:43:02 +0530

    added threading completed project

--- YouTube_Download.py
+++ YouTube_Download.py
@@ -1,3 +1,4 @@
+import threading
 import tkinter
 from pytube import YouTube
 from tkinter import *
@@ -8,7 +9,8 @@ from pathlib import Path
 # download Button Functionality
 screen = Tk()
 screen.title("YouTube Download")
-screen.wm_iconbitmap(r"C:\Users\sahil\Desktop\apps\Projects\YoutubeVideo_downloader\youtube_downloader.ico")
+screen.wm_iconbitmap(
+    r"C:\Users\sahil\Desktop\apps\Projects\YoutubeVideo_downloader\youtube_downloader.ico")
 canvas = Canvas(screen, width=600, height=500)
 canvas.pack()
 
@@ -63,6 +65,7 @@ def download_file():
         text=f"Downloading Your Stuff. Please Wait!", fg='#5ec7f7')
     if choose.get().lower() == 'video':
         # print('videos')
+
         Videos = youtube.streams.filter(
             mime_type='video/mp4', progressive=True)
         Videos.filter(res=format_of_item.get()).first().download(
@@ -82,25 +85,36 @@ def download_file():
     link_field.delete(0, END)
     combobox.set('Select')
 
+    progress['value'] += 25
+
     link_label.config(
-        text=r"Downloading Completed \(U+1F44D)", fg='#22d41c')
+        text=r"Downloading Completed!", fg='#22d41c')
 
 
 def done():
     choice = choose.get().lower()
-    done_btn.wait_visibility()
     youtube = YouTube(link_field.get())
-    progress = ttk.Progressbar(screen,orient=HORIZONTAL,length=100, mode='determinate')
-    canvas.create_window(300, 350, window=progress)
+
     if choice == 'video':
         video(youtube)
     elif choice == 'audio':
         audio(youtube)
     elif choice == 'thumbnail':
         pass
+    elif choice == '':
+        link_label.config(text="Please Choose any option!:: ", fg='red')
     else:
         link_label.config(text="Please Enter Link below!: ", fg='red')
-    
+
+
+def thread():
+    thred = threading.Thread(target=done)
+    thred.start()
+
+
+def thread1():
+    thred = threading.Thread(target=download_file)
+    thred.start()
 
 
 # image logo
@@ -123,14 +137,18 @@ locate_btn = Button(screen, text='Locate', width=10,
 format_of_item = tkinter.StringVar()
 
 # Download Buttons
-done_btn = Button(screen, width=15, text='Done', command=done)
+done_btn = Button(screen, width=15, text='Done', command=thread)
 download_btn = Button(screen, text="Download", width=20, height=2, font=(
-    'summary notes', 12), command=download_file, bg='#2dcc47')
+    'summary notes', 12), command=thread1, bg='#2dcc47')
 
 # Combobox with demo qualitys
 combobox = ttk.Combobox(width=15, state='readonly')
 combobox['values'] = ('1080p', '720p', '480p', '360p', '240p', '144p')
 combobox.set('Select')
+
+# progressbar
+progress = ttk.Progressbar(screen, orient=HORIZONTAL,
+                           length=100, mode='determinate')
 
 # choise by radiobutton
 choose = tkinter.StringVar()
@@ -149,9 +167,9 @@ canvas.create_window(300, 200, window=link_field)
 canvas.create_window(300, 310, window=path_label)
 canvas.create_window(300, 350, window=locate_btn)
 canvas.create_window(300, 450, window=download_btn)
-canvas.create_window(130, 230, window=choice_radio1)
-canvas.create_window(300, 230, window=choice_radio2)
-canvas.create_window(460, 230, window=choice_radio3)
+canvas.create_window(114, 230, window=choice_radio1)
+canvas.create_window(115, 250, window=choice_radio2)
+canvas.create_window(128, 270, window=choice_radio3)
 canvas.create_window(250, 400, window=quality_label)
 
 
